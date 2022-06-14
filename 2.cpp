ec69c1e32138fdb695e402b0c6d19c1f01254830
adumannn <100424533+adumannn@users.noreply.github.com>
2022-06-14 22:08:11 +0600

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for (int i=1; i<=n; i++){
+    	cin>>a[i];
+	}
+    for (int i=1; i<=n; i++){
+        for (int j=i+1; j<=n; j++){
+            if (a[i]>a[j]){
+            	
+                int b = a[i];
+                a[i] = a[j];
+                a[j] = b;
+            }
+            
+        }
+    }
+    for (int i = 1; i <= n; i++){
+        cout<< a[i] << " ";
+    }
+}
